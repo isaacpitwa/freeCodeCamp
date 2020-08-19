8fd7caa9782295714b2e97e20be28ea6aa69f7f8
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-08-19 20:40:21 +0300

    Open

--- fontend-Libraries/random-quote-machine/js/scripts.js
+++ fontend-Libraries/random-quote-machine/js/scripts.js
@@ -9,3 +9,6 @@ function inIframe () {
 
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 var currentQuote = '', currentAuthor = '';
+function openURL(url){
+    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+  }
